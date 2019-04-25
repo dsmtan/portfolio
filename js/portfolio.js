ea0191e253d68ee4eb0e3c95ca72dd9755f269b0
dsmtan <denisetan@live.nl>
2019-04-25 20:33:05 +0200

    added portfolio content

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,15 +1,28 @@
 "use strict";
 
 const menuBtn = document.querySelector("#burgermenu");
+const closeBtn = document.querySelector(".closebtn");
+const mobileNav = document.querySelector("#mobilenav");
 
 menuBtn.addEventListener("click", openNav);
+closeBtn.addEventListener("click", closeNav);
 
 function openNav() {
-  document.getElementById("mobilenav").style.height = "100%";
+  mobileNav.style.height = "100%";
 }
 
 function closeNav() {
-  document.getElementById("mobilenav").style.height = "0%";
+  mobileNav.style.height = "0%";
 }
 
 // in mobile: pf and about content appear on dropdown click or header click
+
+const pfHeader = document.querySelector("#pftext");
+const pfWrapper = document.querySelector("#pfwrapper");
+
+pfHeader.addEventListener("click", togglePortfolio);
+
+function togglePortfolio() {
+  let newHeight = pfWrapper.style.height === "auto" ? "0%" : "auto";
+  pfWrapper.style.height = newHeight;
+}
